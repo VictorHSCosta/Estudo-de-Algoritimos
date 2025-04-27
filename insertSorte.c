0cc5afd32c3ca9f38298736ab5f9b8232a4823e4
VictorHSCosta <victorhenriquecosta23@gmail.com>
2025-04-27 16:19:21 -0300

    Refactor sorting implementation to use merge sort and update main logic for input handling

--- insertSorte.c
+++ insertSorte.c
@@ -1,52 +1,67 @@
 #include <stdio.h>
 
-int main(){
-    int quantidade;
+long long merge_sort(int array[], int inicio, int fim) {
+    if (inicio >= fim) return 0;
 
+    long long contador = 0;
+
+    int meio = (inicio + fim) / 2;
+    contador += merge_sort(array, inicio, meio );
+    contador += merge_sort(array, meio + 1, fim);
+
+    int aux[fim + 1];
+
+    int i = inicio, j = meio + 1, k = inicio;
+    for (int p = inicio; p <= fim; p++) {
+        aux[p] = array[p];
+    }
+
+    while (i <= meio && j <= fim) {
+        if (aux[i] <= aux[j]) {
+            array[k++] = aux[i++];
+        } else {
+            array[k++] = aux[j++];
+            contador += (meio - i + 1);        
+        }
+    }
+
+    while (i <= meio) {
+        array[k++] = aux[i++];
+    }
+
+    return contador;
+}
+
+int main() {
+    int quantidade;
     scanf("%d", &quantidade);
 
-    if(quantidade < 1 || quantidade > 5){
+    if (quantidade < 1 || quantidade > 5) {
         return 0;
     }
 
     int resposta[quantidade];
 
-    for(int i = 0 ; i < quantidade ;i++){
-
+    for (int i = 0; i < quantidade; i++) {
         int tamanho;
-
         scanf("%d", &tamanho);
 
-        if(tamanho < 1 || tamanho > 100000){
+        if (tamanho < 1 || tamanho > 100000) {
             return 0;
         }
 
         int array[tamanho];
 
-        int contador = 0;
-
         for (int j = 0; j < tamanho; j++) {
             scanf("%d", &array[j]);
         }
 
-        for (int j = 1; j < tamanho; j++) {
-            int g = j;
-            while (g > 0 && array[g] < array[g - 1]) {
-                int temp = array[g];
-                array[g] = array[g - 1];
-                array[g - 1] = temp;
-
-                contador++;
-                g--;
-            }
-        }
-
-
-        resposta[i] = contador;
+        resposta[i] = merge_sort(array, 0, tamanho - 1);;
     }
 
-    for(int i = 0 ; i < quantidade ;i++){
-        printf("%d" , resposta[i]);
-        printf("\n");
+    for (int i = 0; i < quantidade; i++) {
+        printf("%d\n", resposta[i]);
     }
+
+    return 0;
 }
